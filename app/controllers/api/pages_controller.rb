14fb7d2c57606bef9e3538ed9e74676f265b50e6
mergitto <merugitosu@gmail.com>
2019-05-21 15:18:03 +0900

    add controller

--- app/controllers/api/pages_controller.rb
+++ app/controllers/api/pages_controller.rb
@@ -1,11 +1,20 @@
 module Api
   class PagesController < ApplicationController
     def index
-      render json: { pages: {} }
+      page = Page.find_by(id: page_params[:page_id])
+      render json: { pages: {}, page: page }
     end
 
     def create
+      p params
       render json: { validation: {}, is_save: {} }
+    end
+
+    private
+
+    def page_params
+      params
+        .permit(:story_id, :page_id)
     end
   end
 end
--- app/controllers/api/stories_controller.rb
+++ app/controllers/api/stories_controller.rb
@@ -1,11 +1,24 @@
 module Api
   class StoriesController < ApplicationController
     def index
-      render json: { stories: {} }
+      render json: Story.all
     end
 
     def create
-      render json: { validation: {}, is_save: {} }
+      story = Story.new(title: story_params['title'], summary: story_params['text'])
+
+      if story.save
+        render json: { validation: {}, story_id: story.id, page_id: 1 }
+      else
+        render json: { validtion: {} }
+      end
+    end
+
+    private
+
+    def story_params
+      params
+        .permit(:title, :text)
     end
   end
 end
