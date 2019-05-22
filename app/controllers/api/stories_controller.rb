9e859410f767fb5611163b1a44b1d874295a24ab
mergitto <merugitosu@gmail.com>
2019-05-22 10:30:26 +0900

    add save page

--- app/controllers/api/stories_controller.rb
+++ app/controllers/api/stories_controller.rb
@@ -8,9 +8,9 @@ module Api
       story = Story.new(title: story_params['title'], summary: story_params['text'])
 
       if story.save
-        story.page.new(name: story_params['title'], text: story_params['text'])
-        if story.page.save
-          render json: { validation: {}, story_id: story.id, page_id: story.page.id }
+        page = story.pages.build(name: story_params['title'], text: story_params['text'])
+        if page.save
+          render json: { validation: {}, story_id: story.id, page_id: page.id }
         end
         render json: { validtion: {} }
       else
