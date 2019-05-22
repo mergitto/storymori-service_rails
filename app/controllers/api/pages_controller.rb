4dfe51fb39c77a299a4d00579190f1f2d74832c8
mergitto <merugitosu@gmail.com>
2019-05-22 16:17:20 +0900

    add page controller for page to create

--- app/controllers/api/pages_controller.rb
+++ app/controllers/api/pages_controller.rb
@@ -1,20 +1,29 @@
 module Api
   class PagesController < ApplicationController
     def index
-      page = Page.find_by(id: page_params[:page_id])
-      render json: { pages: {}, page: page }
+      page = Page
+                .select(:id, :name, :text)
+                .where(id: page_params['page_id'], story_id: page_params['story_id'])
+                .first
+      render json: { id: page.id, name: page.name, text: page.text, children: Page.next_page(page.id) }
     end
 
     def create
-      p params
-      render json: { validation: {}, is_save: {} }
+      page = Page.new(
+        story_id: page_params['story_id'],
+        parent_id: page_params['parent_id'],
+        name: page_params[:page]['name'],
+        text: page_params[:page]['text'],
+      )
+      if page.save
+        render json: page
+      end
     end
 
     private
 
     def page_params
-      params
-        .permit(:story_id, :page_id)
+      params.permit(:story_id, :page_id, :parent_id, page: [:name, :text])
     end
   end
 end
