7643d7a5cff1df5de2f083a2a9466c8ac4c79c63
mergitto <merugitosu@gmail.com>
2019-05-22 18:25:18 +0900

    add error handling

--- app/controllers/api/stories_controller.rb
+++ app/controllers/api/stories_controller.rb
@@ -10,21 +10,24 @@ module Api
     def create
       story = Story.new(title: story_params['title'], summary: story_params['text'])
 
-      if story.save
-        page = story.pages.build(name: story_params['title'], text: story_params['text'])
-        if page.save
-          render json: { storyId: story.id, pageId: page.id }
-        end
-      else
-        render json: {}
+      ActiveRecord::Base.transaction do
+        story.save!
+        page = story.pagesuild(name: story_params['title'], text: story_params['text'])
+        page.save!
+        render json: { storyId: story.id, pageId: page.id }
       end
+      rescue => e
+        render_error_status(e)
     end
 
     private
 
+    def render_error_status(error)
+      render json: { errors: error }, status: 400
+    end
+
     def story_params
-      params
-        .permit(:title, :text)
+      params.permit(:title, :text)
     end
   end
 end
