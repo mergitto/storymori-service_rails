059a9f7ba549fc3e042b674857cd564d69cc746a
mergitto <merugitosu@gmail.com>
2019-05-23 13:45:48 +0900

    fix story test

--- spec/factories/stories.rb
+++ spec/factories/stories.rb
@@ -1,6 +1,5 @@
 FactoryBot.define do
   factory :story do
-    id { 1 }
     title { "Story Test 1" }
     summary { "Story Test Summary" }
   end
--- spec/models/story_spec.rb
+++ spec/models/story_spec.rb
@@ -2,11 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Story, type: :model do
   before do
-    @story = build(:story)
+    @story = create(:story)
   end
 
   it 'correct data' do
-    expect(@story.id).to eq(1)
     expect(@story.title).to eq('Story Test 1')
     expect(@story.summary).to eq('Story Test Summary')
   end
